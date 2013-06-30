b94be4dbc8dc86ea33c2ce089c6b65480c1cf323
Timothy Winters <timothy@timothywinters.com>
2013-06-29 20:25:54 -0700

    completed exercise 26 and working on 27

--- ex26.rb
+++ ex26.rb
@@ -1,44 +1,69 @@
+module Ex25
+
 # This function will break up words for us.
-def break_words(stuff)
+def self.break_words(stuff)
     words = stuff.split(' ')
-    return words
+    words
 end
 
 # Sorts the words.
-def sort_words(words)
-    return sorted(words)
+def self.sort_words(words)
+	words.sort()
 end
 
 # Prints the first word after popping it off.
-def puts_first_word(words)
-    word = words.pop(0)
+def self.puts_first_word(words)
+    word = words.pop()
     puts word
 end
 
 # Prints the last word after popping it off.
-def puts_last_word(words)
-    word = words.pop(-1
+def self.puts_last_word(words)
+    word = words.pop()
     puts word
 end
 
 # Takes in a full sentence and returns the sorted words.
-def sort_sentence(sentence)
+def self.sort_sentence(sentence)
     words = break_words(sentence)
-    return sort_words(words)
+    sort_words(words)
 end
 
 # Puts the first and last words of the sentence.
-def puts_first_and_last(sentence)
+def self.prints_first_and_last(sentence)
     words = break_words(sentence)
     puts_first_word(words)
     puts_last_word(words)
 end
 
 # Sorts the words then prints the first and last one.
-def puts_first_and_last_sorted(sentence)
+def self.print_first_and_last_sorted(sentence)
     words = sort_sentence(sentence)
-    puts_first_word(words)
-    puts_last_word(words)
+    print_first_word(words)
+    print_last_word(words)
+end
+
+sentence = "All good things come to those who wait."
+
+words = Ex25.break_words(sentence)
+sorted_words = Ex25.sort_words(words)
+
+puts_first_word(words)
+puts_last_word(words)
+puts_first_word(sorted_words)
+puts_last_word(sorted_words)
+sorted_words = Ex25.sort_sentence(sentence)
+print sorted_words
+
+def self.print_first_and_last(sentence)
+  print_first_word(words)
+  print_last_word(words)
+end
+
+def self.print_first_and_last_sorted(sentence)
+   words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
 end
 
 
@@ -59,42 +84,24 @@ puts "--------------"
 puts poem
 puts "--------------"
 
-five = 10 - 2 + 3 - 5
+five = 10 - 2 + 3 - 6
 puts "This should be five: %s" % five
+end 
 
-def secret_formula(started)
-    jelly_beans = started * 500
-    jars = jelly_beans \ 1000
+def secret_formula(start_point)
+    jelly_beans = start_point * 500
+    jars = jelly_beans / 1000
     crates = jars / 100
     return jelly_beans, jars, crates
 end
 
 start_point = 10000
-beans, jars, crates == secret_formula(start-point)
+beans, jars, crates = secret_formula(start_point)
 
-puts "With a starting point of: %d" % start_point
-puts "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
+puts "With a starting point of: %s" % start_point
+puts "We'd have #{beans} beans, #{jars} jars, and #{crates} crates." 
 
 start_point = start_point / 10
 
 puts "We can also do that this way:"
-puts "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont
-
-
-sentence = "All god\tthings come to those who weight."
-
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
-
-puts_first_word(words)
-puts_last_word(words)
-.puts_first_word(sorted_words)
-puts_last_word(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
-prin sorted_words
-
-puts_irst_and_last(sentence)
-
-puts_first_a_last_sorted(senence)
-
-
+puts "We'd have %s beans, %s jars, and %s crates." % secret_formula(start_point)
